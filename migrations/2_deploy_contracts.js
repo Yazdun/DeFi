e26d65b2be1d1ee4eb5043ff83f700863a74539c
Yazdun <yazduntube@gmail.com>
2022-03-22 09:24:15 +0430

    commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,22 @@
+const TokenFarm = artifacts.require("TokenFarm");
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
+
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Transfer all tokens to TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, "1000000000000000000000000");
+
+  // Transfer 100 Mock DAI tokens to investor
+  await daiToken.transfer(accounts[1], "100000000000000000000");
+};
